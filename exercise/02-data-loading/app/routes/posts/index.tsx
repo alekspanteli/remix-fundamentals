739b5612427f663b6f8992e4ef09d92ac71aea3f
Alex Panteli <alekspanteli@gmail.com>
2022-10-30 18:23:12 +0200

    test

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,32 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
+
+async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export const loader = async () => {
+  return json({
+    posts: await getPosts(),
+  });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData();
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {/* @ts-expect-error we'll deal with this in extra credit */}
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
